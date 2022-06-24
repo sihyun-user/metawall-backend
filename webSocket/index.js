75e644a3301fdfe4b67d2db098f68d1f8f94bc4d
chsiyu <chsiyu8652@gmail.com>
2022-06-24 17:28:02 +0800

    feat: 新增 webSocket

---
+++ webSocket/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const SocketServer = require('ws').Server
+
+//指定開啟的 port
+const PORT = 8080
+
+//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+//將 express 交給 SocketServer 開啟 WebSocket 的服務
+const wss = new SocketServer({ server })
+
+//當 WebSocket 從外部連結時執行
+wss.on('connection', ws => {
+
+    //連結時執行此 console 提示
+    console.log('Client connected')
+
+		ws.on('message', (message) => {
+			console.log('received: %s', message);
+		})
+
+    //當 WebSocket 的連線關閉時執行
+    ws.on('close', () => {
+      console.log('Close connected')
+    })
+})
